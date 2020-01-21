cc676b03c6d8b8d0e3bd4b225e0a908d6bb52c8d
teripanda <teri.london@gmail.com>
2020-01-21 15:53:16 -0800

    refactored into redux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,69 +1,68 @@
 import React, { useState, useEffect, useReducer } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Gameboard from "../Gameboard";
 import initializeDeck from "../Deck/index.js";
 
-function gameReducer(prevState, action) {
-  switch (action.type) {
-    case "generateCards":
-      return { ...prevState, cards: action.payload };
-    case "disableBoard":
-      return { ...prevState, disabled: true };
-    case "enableBoard":
-      return { ...prevState, disabled: false };
-    case "flippedCards":
-      return { ...prevState, flipped: action.payload };
-    case "solvedCards":
-      return { ...prevState, solved: action.payload };
-    case "resetFlippedCards":
-      return { ...prevState, flipped: initialState.flipped, disabled: false };
-    case "resetGame":
-      return initialState;
-    default:
-      return prevState;
-  }
-}
-
-const initialState = {
-  cards: [],
-  flipped: [],
-  solved: [],
-  disabled: false
-};
-
 function App() {
+  const { cards, flipped, solved, disabled } = useSelector(state => state);
+  const dispatchGameState = useDispatch();
+
   const [dimension, setDimension] = useState(400);
 
-  const [state, dispatch] = React.useReducer(gameReducer, initialState);
-  const { cards, flipped, solved, disabled } = state;
+  //board settings
 
   useEffect(() => {
     resizeBoard();
-    dispatch({ type: "generateCards", payload: initializeDeck() });
+    dispatchGameState({ type: "generateCards", payload: initializeDeck() });
   }, []);
 
-  useEffect(() => {
-    preloadImages();
-  }, cards);
+  // useEffect(() => {
+  //   preloadImages();
+  // }, cards);
 
   useEffect(() => {
     const resizeListener = window.addEventListener("resize", resizeBoard);
     return () => window.removeEventListener("resize", resizeListener);
   });
 
+  // const preloadImages = () => {
+  //   cards.map(card => {
+  //     const src = `/img/${card.type}.jpeg`;
+  //     new Image().src = src;
+  //   });
+  // };
+
+  const resizeBoard = () => {
+    setDimension(
+      Math.min(
+        document.documentElement.clientWidth,
+        document.documentElement.clientHeight
+      )
+    );
+  };
+
+  //game logic
+
   const handleClick = id => {
-    dispatch({ type: "disableBoard" });
+    dispatchGameState({ type: "disableBoard" });
     if (flipped.length === 0) {
-      dispatch({ type: "flippedCards", payload: [id] });
-      dispatch({ type: "enableBoard" });
+      dispatchGameState({ type: "flippedCards", payload: [id] });
+      dispatchGameState({ type: "enableBoard" });
     } else {
       if (sameCardClicked(id)) return;
-      dispatch({ type: "flippedCards", payload: [flipped[0], id] });
+      dispatchGameState({ type: "flippedCards", payload: [flipped[0], id] });
       if (isMatch(id)) {
-        dispatch({ type: "flippedCards", payload: [id] });
-        dispatch({ type: "solvedCards", payload: [...solved, flipped[0], id] });
-        dispatch({ type: "resetFlippedCards" });
+        dispatchGameState({ type: "flippedCards", payload: [id] });
+        dispatchGameState({
+          type: "solvedCards",
+          payload: [...solved, flipped[0], id]
+        });
+        dispatchGameState({ type: "resetFlippedCards" });
       } else {
-        setTimeout(() => dispatch({ type: "resetFlippedCards" }), 2000);
+        setTimeout(
+          () => dispatchGameState({ type: "resetFlippedCards" }),
+          2000
+        );
       }
     }
   };
@@ -74,22 +73,6 @@ function App() {
     const clickedCard = cards.find(card => card.id === id);
     const flippedCard = cards.find(card => flipped[0] === card.id);
     return flippedCard.type === clickedCard.type;
-  };
-
-  const preloadImages = () => {
-    cards.map(card => {
-      const src = `/img/${card.type}.jpeg`;
-      new Image().src = src;
-    });
-  };
-
-  const resizeBoard = () => {
-    setDimension(
-      Math.min(
-        document.documentElement.clientWidth,
-        document.documentElement.clientHeight
-      )
-    );
   };
 
   return (
--- src/reduxReducer.js
+++ src/reduxReducer.js
@@ -1,9 +1,27 @@
-const initialState = {};
+const initialState = {
+  cards: [],
+  flipped: [],
+  solved: [],
+  disabled: false
+};
 
 export default (prevState = initialState, { type, payload }) => {
-  console.log({ type, payload });
   switch (type) {
+    case "generateCards":
+      return { ...prevState, cards: payload };
+    case "disableBoard":
+      return { ...prevState, disabled: true };
+    case "enableBoard":
+      return { ...prevState, disabled: false };
+    case "flippedCards":
+      return { ...prevState, flipped: payload };
+    case "solvedCards":
+      return { ...prevState, solved: payload };
+    case "resetFlippedCards":
+      return { ...prevState, flipped: initialState.flipped, disabled: false };
+    case "resetGame":
+      return initialState;
     default:
-      return { ...prevState };
+      return prevState;
   }
 };
