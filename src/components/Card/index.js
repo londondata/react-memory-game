6006f53964ac32bdb9383e9c322a4e1c5a86ac10
teripanda <teri.london@gmail.com>
2021-06-22 22:25:57 -0700

    minor fixes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,43 +3,44 @@ import PropTypes from "prop-types";
 import "./styles.css";
 
 Card.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  id: PropTypes.number.isRequired,
-  type: PropTypes.string.isRequired,
-  flipped: PropTypes.bool.isRequired,
-  solved: PropTypes.bool.isRequired,
-  height: PropTypes.number.isRequired,
-  width: PropTypes.number.isRequired,
-  disabled: PropTypes.bool.isRequired
+	handleClick: PropTypes.func.isRequired,
+	id: PropTypes.number.isRequired,
+	type: PropTypes.string.isRequired,
+	flipped: PropTypes.bool.isRequired,
+	solved: PropTypes.bool.isRequired,
+	height: PropTypes.number.isRequired,
+	width: PropTypes.number.isRequired,
+	disabled: PropTypes.bool.isRequired,
 };
 
 function Card({
-  handleClick,
-  id,
-  type,
-  flipped,
-  height,
-  width,
-  disabled,
-  solved
+	handleClick,
+	id,
+	type,
+	flipped,
+	height,
+	width,
+	disabled,
+	solved,
 }) {
-  return (
-    <>
-      <div
-        className={`flip-container ${flipped ? "flipped" : ""}`}
-        style={{ width, height }}
-        onClick={() => (disabled ? null : handleClick(id))}
-      >
-        <div className="flipper">
-          <img
-            style={{ height, width }}
-            className={flipped ? "front" : "back"}
-            src={flipped || solved ? `/img/${type}.jpeg` : `/img/cardback.jpeg`}
-          />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className={`flip-container ${flipped ? "flipped" : ""}`}
+				style={{ width, height }}
+				onClick={() => (disabled ? null : handleClick(id))}
+			>
+				<div className="flipper">
+					<img
+						style={{ height, width }}
+						alt="pokemon card"
+						className={flipped ? "front" : "back"}
+						src={flipped || solved ? `/img/${type}.jpeg` : `/img/cardback.jpeg`}
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Card;
