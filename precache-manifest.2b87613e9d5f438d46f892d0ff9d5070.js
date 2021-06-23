12d1a67b927283829c11e6b5501ad803ef387ade
teripanda <teri.london@gmail.com>
2021-06-23 01:01:25 -0700

    Updates

---
+++ precache-manifest.2b87613e9d5f438d46f892d0ff9d5070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c561752b3fa2d4a59c65b032c4118915",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "412efc18bf2089ef21f7",
+    "url": "/react-memory-game/static/css/main.63f64f7a.chunk.css"
+  },
+  {
+    "revision": "fdfc8859918017057e3d",
+    "url": "/react-memory-game/static/js/2.89d19712.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/react-memory-game/static/js/2.89d19712.chunk.js.LICENSE"
+  },
+  {
+    "revision": "412efc18bf2089ef21f7",
+    "url": "/react-memory-game/static/js/main.b4acea60.chunk.js"
+  },
+  {
+    "revision": "9939fee077a796e8ff7d",
+    "url": "/react-memory-game/static/js/runtime-main.b922fa4e.js"
+  }
+]);
